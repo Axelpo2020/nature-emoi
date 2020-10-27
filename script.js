11b9dd72a6fd0a8739392db3c71848f137467020
Axel <axel@MacBook-Pro-de-Axel.local>
2020-10-27 14:56:51 +0100

    test

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Menu responsive
 const btn = document.getElementById("menu-toggle");
+const menu = document.querySelector('.nav-links');
 const lines = btn.querySelectorAll(".line");
 const cls = { open: "open", close: "close" };
 let btnClass = cls.open;
@@ -8,7 +9,9 @@ btn.addEventListener("click", () => {
   if (btn.classList.contains(cls.open)) {
     btn.classList.remove(btnClass);
     btnClass = cls.close;
+    menu.classList.remove('nav-active')
   } else if (btn.classList.contains(cls.close)) {
+    menu.classList.add('nav-active')
     btn.classList.remove(btnClass);
     btnClass = cls.open;
   }
